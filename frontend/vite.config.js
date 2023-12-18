b0e4aa8d053452a4f222a1f93cae0c4a2f712226
Lucas Mazzarino <Mazzanga1212@gmail.com>
2023-12-18 19:01:27 -0300

    ,

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  base: '/portfolio/', 
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-};
+})
